3d88f1f8612ece554900ae69370562738f4590cd
imadkasse <kasseimad81@gmail.com>
2025-09-06 18:04:38 +0100

    fixing errors

--- app/dashboard-teacher/layout.tsx
+++ app/dashboard-teacher/layout.tsx
@@ -16,6 +16,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const user = await fetchUserServer();
+  console.log("user : dashboard-teacher", user);
   if (!user) {
     redirect("/login");
   }
