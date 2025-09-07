90deed82b0b69d1916f4aa817ba395ca113c62d9
imadkasse <kasseimad81@gmail.com>
2025-09-07 17:03:08 +0100

    fixing errors

--- lib/fetchUserServer.ts
+++ lib/fetchUserServer.ts
@@ -1,9 +1,4 @@
-import { cookies } from "next/headers";
-
 export async function fetchUserServer() {
-  const cookiesStore = await cookies();
-  const token = cookiesStore.get("token")?.value;
-
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BACK_URL}/api/users/me`,
