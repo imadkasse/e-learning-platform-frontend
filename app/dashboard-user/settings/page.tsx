0617de0624989b60ec3d71a1e3c243366e9ca93f
imadkasse <kasseimad81@gmail.com>
2025-09-10 16:14:26 +0100

    read token from cookie

--- app/dashboard-user/settings/page.tsx
+++ app/dashboard-user/settings/page.tsx
@@ -1,13 +1,13 @@
 import Settings from "@/components/dashboard-user/settings/Settings";
-// import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 import React from "react";
 const fetchingUser = async () => {
-  // const cookieStore = await cookies();
-  // const token = cookieStore.get("token")?.value;
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
   try {
     const res = await fetch(`${process.env.BACK_URL}/api/users/me`, {
       credentials: "include",
-      // headers: token ? { Authorization: `Bearer ${token}` } : {},
+      headers: token ? { Authorization: `Bearer ${token}` } : {},
       cache: "no-store",
     });
     console.log("res status", res.status);
